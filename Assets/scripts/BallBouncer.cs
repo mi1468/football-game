34e5c9e8edd6d62f2339703a2ab5c1ab62eee669
mahdi <mi1468@yahoo.com>
2021-12-04 21:43:07 +0330

    learn unity 7 - unity bounce

---
+++ Assets/scripts/BallBouncer.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class BallBouncer : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Just for debugging, adds some velocity during OnEnable")]
+    private Vector3 initialVelocity;
+
+    [SerializeField]
+    private float minVelocity = 0;
+
+    private Vector3 lastFrameVelocity;
+    private Rigidbody rb;
+    
+    private void OnEnable()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = initialVelocity;
+    }
+
+    private void Update()
+    {
+        lastFrameVelocity = rb.velocity;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Player")
+            Bounce(collision.contacts[0].normal);
+    }
+
+    private void Bounce(Vector3 collisionNormal)
+    {
+        var speed = lastFrameVelocity.magnitude;
+        var direction = Vector3.Reflect(lastFrameVelocity.normalized, collisionNormal);
+
+        Debug.Log("Out Direction: " + direction);
+        rb.velocity = direction * Mathf.Max(speed, minVelocity);
+    }
+}
