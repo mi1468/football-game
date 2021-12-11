80f9ef6658a5a2be7e2d80af39855f82037ad3c7
mahdi <mi1468@yahoo.com>
2021-12-11 10:29:01 +0330

    learn unity 8,9

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class player : MonoBehaviour
 {
-    public GameObject target;
-    Vector3 lastpos, firstpos, chenge;
+    public GameObject target, arrow;
+    public Vector3 firstarrowsize, lastpos, firstpos, chenge;
     public float power = 1;
+    public float maxdistance = 130;
+    public bool havetarget = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,15 +18,30 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(havetarget)
+        {
+            Vector2 mouse = Input.mousePosition;
+            Vector3 chenge = new Vector3(firstpos.x - mouse.x, 0, firstpos.y - mouse.y);
+            if(chenge.magnitude > maxdistance)
+            {
+                chenge = chenge.normalized * maxdistance;
+            }
+            arrow.transform.localScale = firstarrowsize +  chenge.magnitude / 400 * new Vector3(1, 4, 1);
+            arrow.transform.LookAt(chenge);
+            arrow.SetActive(true);
+        }
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out hit, 100.0f))
+        if (Physics.Raycast(ray, out hit, 100.0f) && Input.GetMouseButtonDown(0))
         {
             if (hit.transform != null)
             {
                 if (hit.transform.GetComponent<Rigidbody>())
                 {
                     target = hit.transform.gameObject;
+                    arrow = target.transform.GetChild(0).gameObject;
+                    havetarget = true;
+                    
                     firstpos = Input.mousePosition;
                 }
             }
@@ -35,7 +52,14 @@ public class player : MonoBehaviour
         {
             lastpos = Input.mousePosition;
             chenge = firstpos - lastpos;
+            if (chenge.magnitude > maxdistance)
+            {
+                chenge = chenge.normalized * maxdistance;
+            }
             chenge = power * chenge;
+            havetarget = false;
+            arrow.SetActive(false);
+
             target.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(chenge.x, 0, chenge.y));
         }
     }
